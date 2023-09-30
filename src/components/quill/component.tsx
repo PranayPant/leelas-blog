8930d354aa22c255ab7cd0be389dcafd4861a864
PranayPant <ppantg1@gmail.com>
2023-09-30 14:47:27 -0500

    feat: override specific tailwind styles for react quill page

--- src/components/quill/component.tsx
+++ src/components/quill/component.tsx
@@ -4,5 +4,10 @@ import "react-quill/dist/quill.snow.css"
 
 export default function QuillComponent() {
   const [value, setValue] = React.useState("")
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />
+  return (
+    <div className="whitespace-pre m-3">
+      <ReactQuill className="h-30 bg-slate-200" theme="snow" value={value} onChange={setValue} />
+      <div dangerouslySetInnerHTML={{ __html: value }} />
+    </div>
+  )
 }
